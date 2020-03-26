0b0cc8cc4c54cf2457fa07def6f5e66f34ac6702
dptrandai <57727830+dptrandai@users.noreply.github.com>
2020-03-25 21:32:34 -0500

    add db and npm installs

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ("cheese burger", true);
+INSERT INTO burgers (burger_name, devoured) VALUES ("Whataburger", true);
+INSERT INTO burgers (burger_name, devoured) VALUES ("IN-N-OUT burger animal style", false);
